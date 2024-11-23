bda3a581564dd30c5f90ff293c5873810727d80c
Matthew Torres <torresmatt99@gmail.com>
2024-11-23 10:33:34 -0500

    Update rocket_spawner.gd

--- rocket_spawner.gd
+++ rocket_spawner.gd
@@ -24,15 +24,16 @@ func _process(delta):
 			spawn()
 			if get_child_count() < max_spawn and spawn_rate < 4:
 				var rand = randi_range(0, 10)
+				if spawn_rate < .5:
+					rand += 1
+				#second spawn
 				if rand > 4:
+					spawn()
 					#tripple spawn
 					if get_child_count() < max_spawn - 1:
-						if rand > 6 and spawn_rate < 4:
+						if rand > 5 and spawn_rate < 4:
 							spawn()
-						elif rand > 4 and spawn_rate < 2:
-							spawn()
-					#second spawn
-					spawn()
+			
 		spawn_tick = spawn_rate
 
 func spawn():
